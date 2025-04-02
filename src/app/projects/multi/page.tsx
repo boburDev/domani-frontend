6f1b9b8fc4c7c17d6fbc33dbe7b547ddc3440ac1
Boburmirzo Negmatov <negmatov1907@gmail.com>
2025-04-02 18:46:42 +0500

    ,

---
+++ src/app/projects/multi/page.tsx
@@ -0,0 +1,7 @@
+export default function MultiFloor() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Ko‘p qavatli binolar</h1>
+    </div>
+  );
+}
---
+++ src/app/social/page.tsx
@@ -0,0 +1,7 @@
+export default function Social() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Ijtimoiy tarmoqlar</h1>
+    </div>
+  );
+}
