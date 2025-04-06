e6bed18e6d0aa791b4eb18c20dc20986e13efbff
Temur_ <145855199+Temur00@users.noreply.github.com>
2025-04-06 20:42:01 +0500

    some changes added

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,21 +1,59 @@
-import React from "react";
+// import React from "react";
 
-const Card = () => {
+// const Card = () => {
+//   return (
+//     <div className="relative w-[335px] h-[238px] lg:w-[546px] lg:h-[441px] rounded-xl overflow-hidden group shadow-md">
+//       <div className="w-full h-full">
+//         <img
+//           src="/forCard.png"
+//           alt="Card"
+//           className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+//         />
+//       </div>
+
+//       <div className="absolute inset-0 group-hover:bg-gradient-to-t from-black/70 via-transparent to-transparent z-10" />
+
+//       <div className="absolute w-full bottom-8 left-1/2 transform -translate-x-1/2 z-20 text-center">
+//         <p className="text-textWhite font-bold text-[24px] opacity-0 translate-y-4 transition-all duration-500 ease-out group-hover:opacity-100 group-hover:translate-y-0">
+//           Marvid mavzesi
+//         </p>
+//         <p className="text-[20px] text-iron opacity-0 translate-y-4 transition-all duration-500 ease-out group-hover:opacity-100 group-hover:translate-y-0 mt-1">
+//           Toshkent sh., Qo'yliq dahasi 5
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Card;
+
+type CardProps = {
+  id?: number;
+  name: string;
+  location: string;
+  img: string;
+  type?: string;
+};
+
+const Card = ({ name, location, img }: CardProps) => {
   return (
-    <div className="relative bg-cover bg-[url('/forCard.png')] w-[546px]  h-[446px]">
-      <div
-        className="absolute inset-0 z-0"
-        style={{
-          background:
-            "linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.7) 100%)",
-        }}
-      />
-      <div className="absolute w-full bottom-8 left-1/2 transform -translate-x-1/2 z-10 text-center">
-        <p className="text-textWhite font-bold text-[24px] transition-all duration-500 ease-out group-hover:translate-y-[-10px]">
-          Marvid mavzesi
+    <div className="relative w-[335px] h-[238px] lg:w-[546px] lg:h-[441px] rounded-xl overflow-hidden group shadow-md">
+      <div className="w-full h-full">
+        <img
+          src={img}
+          alt="Card"
+          className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+        />
+      </div>
+
+      <div className="absolute inset-0 group-hover:bg-gradient-to-t from-black/70 via-transparent to-transparent z-10" />
+
+      <div className="absolute w-full bottom-8 left-1/2 transform -translate-x-1/2 z-20 text-center">
+        <p className="text-textWhite font-bold text-[18px] sm:text-[24px] opacity-0 translate-y-4 transition-all duration-500 ease-out group-hover:opacity-100 group-hover:translate-y-0">
+          {name}
         </p>
-        <p className="text-[20px] text-iron opacity-0 translate-y-4 transition-all duration-500 ease-out group-hover:opacity-100 group-hover:translate-y-0">
-          Toshkent sh., Qo'yliq dahasi 5
+        <p className="text-[14px] sm:text-[20px] text-iron opacity-0 translate-y-4 transition-all duration-500 ease-out group-hover:opacity-100 group-hover:translate-y-0 mt-1">
+          {location}
         </p>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,37 +10,37 @@ module.exports = {
     extend: {
       fontFamily: {
         poppins: ["Poppins"],
-        playfair: ["playfairDisplay"],
+        playfair: ["Playfair Display"],
       },
-    },
-    screens: {
-      sm: "480px", // Small screens (phones in portrait mode)
-      md: "768px", // Tablets in portrait mode
-      lg: "1024px", // Tablets in landscape mode
-      xl: "1440px", // Large screens (laptops, desktops)
-      xxl: "1650px", // Extra large screens
-      "2xl": "1920px", // Full HD (base size)
-    },
-    container: {
-      center: true,
-      padding: "4px",
+      colors: {
+        rangoonGreen: "#1A1A1A",
+        black: "#000000",
+        textBlack: "#1E1E1E",
+        textWhite: "#FFFFFF",
+        lightGrey: "#D9D9D9",
+        gainsboro: "#DFDFDF",
+        iron: "#D8D8D8",
+      },
       screens: {
-        sm: "100%",
-        md: "730px",
-        lg: "960px",
-        xl: "1120px",
-        xxl: "1300px",
-        "2xl": "1650px",
+        sm: "480px", // Small screens (phones in portrait mode)
+        md: "768px", // Tablets in portrait mode
+        lg: "1024px", // Tablets in landscape mode
+        xl: "1440px", // Large screens (laptops, desktops)
+        xxl: "1650px", // Extra large screens
+        "2xl": "1920px", // Full HD (base size)
       },
-    },
-    colors: {
-      rangoonGreen: "#1A1A1A",
-      black: "#000000",
-      textBlack: "#1E1E1E",
-      textWhite: "#FFFFFF",
-      lightGrey: "#D9D9D9",
-      gainsboro: "#DFDFDF",
-      iron: "#D8D8D8",
+      container: {
+        center: true,
+        padding: "4px",
+        screens: {
+          sm: "100%",
+          md: "730px",
+          lg: "960px",
+          xl: "1120px",
+          xxl: "1300px",
+          "2xl": "1650px",
+        },
+      },
     },
   },
   plugins: [],
