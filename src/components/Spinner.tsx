93e391ba5c39de2d15aad6aabb2afdbf80a18a3b
Temur_ <145855199+Temur00@users.noreply.github.com>
2025-04-10 23:19:01 +0500

    Update Spinner.tsx

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Spinner: React.FC = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex z-50 justify-center items-center h-screen">
       <div className="border-t-4 border-blue-500 border-solid rounded-full w-16 h-16 animate-spin"></div>
     </div>
   );
